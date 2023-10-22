22bae78b4700a3bb01760a5f3fcd085e8fb6a010
venediktova-kate <venediktova.kat@gmail.com>
2023-10-22 22:50:44 +0700

    Make migrate

--- migrations/env.py
+++ migrations/env.py
@@ -5,7 +5,7 @@ from sqlalchemy import pool
 
 from alembic import context
 
-from core.config import DB_USER, DB_PASS, DB_NAME, DB_HOST
+from core.database import DB_USER, DB_PASS, DB_NAME, DB_HOST
 from core.database import Base
 
 # this is the Alembic Config object, which provides
