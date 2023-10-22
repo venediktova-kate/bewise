5bf36311f639349ef3dcf34daf7033db4cd6101e
venediktova-kate <venediktova.kat@gmail.com>
2023-10-22 22:50:23 +0700

    Add dotenv

--- core/database.py
+++ core/database.py
@@ -1,7 +1,18 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-from config import DB_HOST, DB_PORT, DB_USER, DB_NAME, DB_PASS
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+DB_HOST = os.environ.get("DB_HOST")
+DB_PORT = os.environ.get("DB_PORT")
+DB_NAME = os.environ.get("DB_NAME")
+DB_USER = os.environ.get("DB_USER")
+DB_PASS = os.environ.get("DB_PASS")
+
 
 SQLALCHEMY_DATABASE_URL = f"postgresql://{DB_USER}:{DB_PASS}@{DB_HOST}:{DB_PORT}/{DB_NAME}"
 
